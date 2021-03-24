fd3388680fdeaa7ea8c2178049b11b46955895c3
Rajkumar Subramaniam <ras4@microsoft.com>
2021-03-24 16:50:16 +0530

    Reply to Nth mail

--- FHLVoiceSearch/FHLVoiceSearch/SpeechDecorator/ReplyDecorator.cs
+++ FHLVoiceSearch/FHLVoiceSearch/SpeechDecorator/ReplyDecorator.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Office.Interop.Outlook;
 using System;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace FHLVoiceSearch.SpeechDecorator
@@ -19,12 +20,37 @@ namespace FHLVoiceSearch.SpeechDecorator
             return this.speechParser.ParseSpeechText(speechText);
         }
 
+        private string GetDigits(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return s;
+            string cleaned = new string(s.Where(char.IsDigit).ToArray());
+            return cleaned;
+        }
+
         public void PerformAction(string speechText)
         {
-            var item = Globals.ThisAddIn.Application.ActiveExplorer().Selection[1];
+            speechText = speechText.Trim();
+            if (speechText.StartsWith("Reply"))
+            {
+                speechText = speechText.Substring(5);
+            }
+
+            speechText = GetDigits(speechText);
+
+
+            bool isParsable = Int32.TryParse(speechText, out int index);
+            if (!isParsable)
+            {
+                index = 1;
+            }
+            Globals.ThisAddIn.Application.ActiveExplorer().SelectAllItems();
+            var item = Globals.ThisAddIn.Application.ActiveExplorer().Selection[index];
+            
             if (item is MailItem)
             {
                 MailItem mailItem = (MailItem)item;
+                // To-do: Explore if a mail can be flaged
+                // mailItem.MarkAsTask(OlMarkInterval.olMarkNoDate);
                 MailItem replyItem = mailItem.Reply();
 
                 replyItem.Display(true);
@@ -33,6 +59,16 @@ namespace FHLVoiceSearch.SpeechDecorator
             {
                 Console.WriteLine(item.GetType());
             }
+
+            /*
+            // Todo: unselect selection
+            int length = Globals.ThisAddIn.Application.ActiveExplorer().Selection.Count;
+            for (int i=1; i < length; i++)
+            {
+                MailItem mailItem = (MailItem) Globals.ThisAddIn.Application.ActiveExplorer().Selection[i]; ;
+                Globals.ThisAddIn.Application.ActiveExplorer().RemoveFromSelection(mailItem);
+            }
+            */
         }
     }
 }
